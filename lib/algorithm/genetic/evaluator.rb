5f41557a6719253f0d2a9a350ef69c4ed90bb690
TADA Tadashi <t@tdtds.jp>
2014-05-09 17:23:24 +0900

    add Evaluator class

---
+++ lib/algorithm/genetic/evaluator.rb
@@ -0,0 +1,19 @@
+
+module Algorithm
+	module Genetic
+		#
+		# = evaluate fitness of a gene
+		#
+		class Evaluator
+			#
+			# evaluate fitness, implement by yourself
+			#
+			# gene :: a instance of Gene class
+			#
+			def fitness(gene)
+				raise NotImplementedError.new('implement fitness method by yourself.')
+			end
+		end
+	end
+end
+
