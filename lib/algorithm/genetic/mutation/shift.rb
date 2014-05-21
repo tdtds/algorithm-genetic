2dd3938ce1004072ac0542b2a40be6f401ad7741
TADA Tadashi <t@tdtds.jp>
2014-05-21 21:17:21 +0900

    return original code when invalid shift

--- lib/algorithm/genetic/mutation/shift.rb
+++ lib/algorithm/genetic/mutation/shift.rb
@@ -4,7 +4,11 @@ module Algorithm::Genetic::Mutation
 		def mutate(code)
 			index = (rand * code.length).to_i
 			direction = rand <= 0.5 ? -1 : 1
-			new_char = (code[index].ord + direction).chr rescue return
+			begin
+				new_char = (code[index].ord + direction).chr
+			rescue
+				return(code)
+			end
 			code[index] = new_char
 			return code
 		end
